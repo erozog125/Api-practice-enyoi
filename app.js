59bc5a5c5ad051221ce373a6f83c3b343380c7da
Edwin Rozo Gómez <erozog125@gmail.com>
2023-12-01 20:12:43 -0500

    Create products

--- app.js
+++ app.js
@@ -1,42 +1,28 @@
-const url = 'https://thesimpsonsquoteapi.glitch.me/quotes'
+const url = 'https://fakestoreapi.com/products'
 
-function renderCharacters() {
-  for (let index = 0; index <= 20; index++) {
-    getCharacter()  
-  }
-}
-
-function getCharacter() {
-    // get:obtener
-    // JSON: javascript object notation
-    fetch(url)
-    .then(response => response.json())
-    .then( data => makeCard(data[0]))
+  const getCharacter = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    data.forEach( element => makeCard(element) )
   }
   
-  // const getCharacter = async () => {
-  //   const response = await fetch(url)
-  //   const data = await response.json()
-  //   makeCard(data[0])
-  // }
-  
-  function makeCard(character) {
+  function makeCard(product) {
     const card = document.createElement('div')
-  card.classList.add('card')
-  const imgCard = document.createElement('img')
-  imgCard.src = character.image
-  imgCard.alt = character.character
-  const nameCard = document.createElement('h2')
-  nameCard.textContent = character.character
-  const quoteCard = document.createElement('p')
-  quoteCard.textContent = character.quote
-  
-  card.appendChild(imgCard)
-  card.appendChild(nameCard)
-  card.appendChild(quoteCard)
-  
-  document.querySelector('body').appendChild(card)
+    card.classList.add('card')
+    const imgCard = document.createElement('img')
+    imgCard.src = product.image
+    imgCard.alt = product.title
+    const nameCard = document.createElement('h2')
+    nameCard.textContent = product.title
+    const descriptionCard = document.createElement('p')
+    descriptionCard.textContent = product.description
+    
+    card.appendChild(imgCard)
+    card.appendChild(nameCard)
+    card.appendChild(descriptionCard)
+    
+    document.querySelector('body').appendChild(card)
   
 }
 
-window.addEventListener('DOMContentLoaded', renderCharacters)
+window.addEventListener('DOMContentLoaded', getCharacter)
